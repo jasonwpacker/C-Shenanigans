d2049c70e13ba2701220b5427721276dad6b56f3
Jason Packer <jasonpacker@jasnmis.com>
2023-12-14 21:34:07 -0600

    Increased number of prints to see changes better

--- linktester.c
+++ linktester.c
@@ -4,8 +4,11 @@
 
 int main() {
     node_link *head = NULL;
+    print_list(head);
     head = add_node_front(head, 12);
+    print_list(head);
     head = add_node_front(head, 7);
+    print_list(head);
     add_node_rear(head, 1);
     print_list(head);
     remove_nth_item(head, 2);
